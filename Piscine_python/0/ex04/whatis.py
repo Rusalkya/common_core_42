2b3ac24aa7d2ed8ebc1e0d648da1ff5b59e89be8
Rusalkya <150519656+Rusalkya@users.noreply.github.com>
2026-06-03 19:22:56 +0000

    ex04 a finir

---
+++ Piscine_python/0/ex04/whatis.py
@@ -0,0 +1,24 @@
+import sys
+
+def main():
+    if len(sys.argv) == 1:
+        return
+
+    if len(sys.argv) > 2:
+        raise AssertionError("more than one argument is provided")
+
+    arg = sys.argv[1]
+
+    try:
+        number = int(arg)
+    except ValueError:
+        raise AssertionError("argument is not an integer")
+
+    if number % 2 == 0:
+        print("I'm Even.")
+    else:
+        print("I'm Odd.")
+
+
+if __name__ == "__main__":
+    main()
